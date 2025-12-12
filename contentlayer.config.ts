7f19af4d71acd9ee74bf44cff66561a526761413
Dave Goosem <davidgoosem@aceik.com.au>
2025-12-12 23:23:05 +1000

    ensure extenral links are secure and parse solution for rule for lint/prettier

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -19,6 +19,7 @@ import rehypeKatex from 'rehype-katex'
 import rehypeCitation from 'rehype-citation'
 import rehypePrismPlus from 'rehype-prism-plus'
 import rehypePresetMinify from 'rehype-preset-minify'
+import rehypeExternalLinks from 'rehype-external-links'
 import siteMetadata from './data/siteMetadata'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer.js'
 
@@ -149,6 +150,7 @@ export default makeSource({
       [rehypeCitation, { path: path.join(root, 'data') }],
       [rehypePrismPlus, { defaultLanguage: 'js', ignoreMissing: true }],
       rehypePresetMinify,
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
     ],
   },
   onSuccess: async (importData) => {
